905f325da6229828fa1e3eeb0b008329abd191ea
Adrien Le Saux <adrien.le-saux@isen-ouest.yncrea.fr>
2019-03-22 14:29:51 +0100

    Directory structure

--- View/view.php
+++ View/view.php
@@ -1,14 +1,3 @@
 <?php
-<<<<<<< HEAD
-/**
- * Created by PhpStorm.
- * User: adrien
- * Date: 22/03/19
- * Time: 14:26
- */
 
-echo "Same name";
-=======
-
-echo "<h1>View</h1>";
->>>>>>> origin/master
+echo "Yo";
