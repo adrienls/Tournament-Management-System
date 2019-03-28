30ac543bbe2e7ca3eaac8ad4c6cbae5cbc6eca6b
arthur <arthur.guerineau@isen-ouest.yncrea.fr>
2019-03-28 15:09:20 +0100

    update of the login stage

--- Controller/login.php
+++ Controller/login.php
@@ -5,11 +5,60 @@ if(isset($_GET['func'])) {
         $_GET['func']($_GET['id']);
     }
     else {
+        //echo "Yes! C'est bon! <br>";
         $_GET['func']();
     }
 }
-if(isset($_GET['func'])){
-    echo "Yes! C'est bon! <br>";
-    $_GET['func']();
-}
 
+function login(){
+    require_once "../Model/DbConfig.php";
+    require_once "redirect.php";
+    $dbConfig=new DbConfig("localhost","Tournament-Management-System","testUser", "testPassword");
+    $dbConfig->connectDB();
+    $connection=$dbConfig->getDbConnexion();
+    //Test si le login et password ne sont pas vide
+
+    if (empty($_POST['login']) || empty($_POST['password'])) {
+        redirect("../View/login.php?error=bad_login");
+        echo"<br>";
+    }
+    $login=$_POST['login'];
+    $dblogin = $connection->prepare("SELECT username FROM Admin");
+    $dblogin->execute();
+    $dblogin = $dblogin->fetchAll();
+    var_dump($dblogin);
+//echo $dblogin2[1]['login'];
+    $dbpassword=NULL;
+    foreach ($dblogin as $log ){
+        echo $log['username'];
+        if($log['username']==$login){
+            //echo "easy";
+            $dbpassword = $connection->prepare("SELECT * FROM Admin WHERE username='$login'");
+            $dbpassword->execute();
+            $dbpassword = $dbpassword->fetch();
+            //var_dump($dbpassword);
+            echo $dbpassword['password'];
+            //header('Location: newuser.php');
+        }
+    }
+
+    if(password_verify($_POST['password'],$dbpassword['password'])){
+        //if($dbpassword['password']==$_POST['password']){
+        session_start();
+        $_SESSION['username'] = $dbpassword['username'];
+        $_SESSION['id'] = $dbpassword['id'];
+        redirect("../View/adminView.php");
+    }
+    if($_POST['password']==$dbpassword['password']){
+        //if($dbpassword['password']==$_POST['password']){
+        session_start();
+        $_SESSION['username'] = $dbpassword['username'];
+        $_SESSION['id'] = $dbpassword['id'];
+        redirect("../View/adminView.php");
+    }
+    /*
+    else{
+        redirect("../View/login.php?error=bad_logine");
+        echo"<br>";
+    }*/
+}
